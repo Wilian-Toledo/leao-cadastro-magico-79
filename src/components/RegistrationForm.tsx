b3a8e70dbd3762375f76dd31eaae2494ba0b2b35
Wilian Toledo <89874269+Wilian-Toledo@users.noreply.github.com>
2025-08-14 15:23:18 -0300

    Update RegistrationForm.tsx

--- src/components/RegistrationForm.tsx
+++ src/components/RegistrationForm.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import * as z from "zod";
 import InputMask from "react-input-mask";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
