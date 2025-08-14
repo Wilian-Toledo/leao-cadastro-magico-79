a2fbac63bae814c8fc09527b70736e4279a321ed
Wilian Toledo <89874269+Wilian-Toledo@users.noreply.github.com>
2025-08-14 15:36:32 -0300

    Update RegistrationForm.tsx

--- src/components/RegistrationForm.tsx
+++ src/components/RegistrationForm.tsx
@@ -4,7 +4,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import InputMask from "react-input-mask";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage, FormDescription } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { Checkbox } from "@/components/ui/checkbox";
